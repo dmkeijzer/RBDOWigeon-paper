2bbe06cbf1357d970886828b04f9d3bdea1bc63c
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-05-08 20:52:47 +0200

    created a file for batteries, it doesn't have anything yet, it was to set up pycharm mostly

---
+++ PropandPower/battery.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+class Battery:
+    """This class is to estimate the parameters of a battery"""
+    def __init__(self,sp_en_den, energy):
+        self.sp_en_den = sp_en_den
+        self.energy = energy
+
+    def mass(self):
+        return self.energy / self.sp_en_den
