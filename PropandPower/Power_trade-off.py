86a23506db5aeef8249d899a70f42a73bb61847d
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-05-10 16:21:03 +0200

    Updated trade-off code

--- PropandPower/Power_trade-off.py
+++ PropandPower/Power_trade-off.py
@@ -1,21 +1,24 @@
+"""
+Trade-off of the power system
+"""
 
 # Weights of the criteria
 weights = {"Specific energy density": 1,
-           "Volumetric energy density": 1,
-           "Power density": 1,
-           "Safety score": 1,
-           "Operational life": 1,
-           "Cost": 1}
+           "Volumetric energy density": 0.8,
+           "Power density": 0.8,
+           "Safety score": 0.5,
+           "Operational life": 0.3,
+           "Cost": 0.3}
 
-# List of batteries
-li_ion = {"Specific energy density": 1,
+# List of power system options
+li_ion = {"Specific energy density": 250,
           "Volumetric energy density": 1,
           "Power density": 1,
           "Safety score": 1,
           "Operational life": 1,
           "Cost": 1}
 
-li_S = {"Specific energy density": 1,
+li_S = {"Specific energy density": 350,
         "Volumetric energy density": 1,
         "Power density": 1,
         "Safety score": 1,
@@ -38,12 +41,12 @@ H2_fuelcell = {"Specific energy density": 1,
 
 
 def score(system):
-    return weights["Specific energy density"]*system["Specific energy density"] + \
-           weights["Volumetric energy density"]*system["Volumetric energy density"] + \
-           weights["Power density"]*system["Power density"] + \
-           weights["Safety score"]*system["Safety score"] + \
-           weights["Operational life"]*system["Operational life"] + \
-           weights["Cost"]*(1/system["Cost"])
+    return weights["Specific energy density"]*system["Specific energy density"] / li_ion["Specific energy density"] + \
+           weights["Volumetric energy density"]*system["Volumetric energy density"] / li_ion["Volumetric energy density"] + \
+           weights["Power density"]*system["Power density"] / li_ion["Power density"] + \
+           weights["Safety score"]*system["Safety score"] / li_ion["Safety score"] + \
+           weights["Operational life"]*system["Operational life"] / li_ion["Operational life"] + \
+           weights["Cost"]*(1/system["Cost"]) / (1/li_ion["Cost"])
 
 
 print("Trade-off scores:")
