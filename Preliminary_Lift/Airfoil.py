6c650ee46fe98884fa5e65e0040cbf7078badf9e
nsalvadorlopez <nsalvadorlopez@student.tudelft.nl>
2021-05-10 12:44:08 +0200

    Airfoil.py main_preliminary_aero.py

--- Preliminary_Lift/Airfoil.py
+++ Preliminary_Lift/Airfoil.py
@@ -3,7 +3,6 @@ import numpy as np
 # WING PLANFORM
 """"
 b =span
-
 """
 def wing_planform(AR,S, sweepc4, taper):
 
@@ -20,7 +19,7 @@ def wing_planform(AR,S, sweepc4, taper):
 
 def wing_planform_double(AR, S1, sweepc41, taper1, S2, sweepc42, taper2):
     #Wing 1
-    b1 = np.sqrt(2* A * S1)
+    b1 = np.sqrt(2* AR * S1)
     c_r1 = 2 * S1 / ((1 + taper1) * b1)
     c_t1 = taper1 * c_r1
     c_MAC1 = (2 / 3) * c_r1 * ((1 + taper1 + taper1 ** 2) / (1 + taper1))
