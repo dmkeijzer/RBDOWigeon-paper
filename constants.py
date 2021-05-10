8847c5f7f0bac91d1244ea8e5b90288069fc410d
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-05-10 15:18:15 +0200

    Created a json file per config

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 from json import load
 
-consts = (lambda f: {co: c[co] for c in load(open(f, 'r')).values() for co in c})('inputs.json')
+consts = (lambda f: {co: c[co] for c in load(open(f, 'r')).values() for co in c})('inputs_config_1.json')
 
 globals().update(consts)
 del consts
