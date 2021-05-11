9af1b29e945ef9efbf304da67f40bb5b9b3201f3
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-05-11 11:34:41 +0200

    Updated trade-off values

--- PropandPower/Power_trade-off.py
+++ PropandPower/Power_trade-off.py
@@ -12,18 +12,28 @@ weights = {"Specific energy density": 1,
 
 # List of power system options
 li_ion = {"Specific energy density": 250,
-          "Volumetric energy density": 1,
-          "Power density": 1,
+          "Volumetric energy density": 650,
+          "Power density": 1000,
           "Safety score": 1,
-          "Operational life": 1,
-          "Cost": 1}
+          "Operational life": 1000,
+          "Cost": 80}  # we expect the price of an average battery pack to be around $94/kWh by 2024 and $62/kWh by 2030
+# https://about.bnef.com/blog/behind-scenes-take-lithium-ion-battery-prices/
+# 125 -> https://www.osti.gov/pages/servlets/purl/1400215
 
-li_S = {"Specific energy density": 350,
-        "Volumetric energy density": 1,
-        "Power density": 1,
+li_S = {"Specific energy density": 550,
+        "Volumetric energy density": 650,
+        "Power density": 11000,
         "Safety score": 1,
-        "Operational life": 1,
-        "Cost": 1}
+        "Operational life": 800,
+        "Cost": 87}  # with Li-S potentially available at about €72 per kWh – 30% less than comparable Li-ion technology
+# https://horizon-magazine.eu/article/cheaper-lighter-and-more-energy-dense-promise-lithium-sulphur-batteries.html
+
+li_metal = {"Specific energy density": 450,
+            "Volumetric energy density": 1000,
+            "Power density": 1,
+            "Safety score": 5,
+            "Operational life": 1,
+            "Cost": 1}
 
 solid_st = {"Specific energy density": 1,
             "Volumetric energy density": 1,
@@ -32,12 +42,19 @@ solid_st = {"Specific energy density": 1,
             "Operational life": 1,
             "Cost": 1}
 
-H2_fuelcell = {"Specific energy density": 1,
-               "Volumetric energy density": 1,
-               "Power density": 1,
-               "Safety score": 1,
-               "Operational life": 1,
-               "Cost": 1}
+GH2_fuelcell = {"Specific energy density": 1,
+                "Volumetric energy density": 1,
+                "Power density": 1,
+                "Safety score": 1,
+                "Operational life": 1,
+                "Cost": 1}
+
+LH2_fuelcell = {"Specific energy density": 1500,
+                "Volumetric energy density": 1000,
+                "Power density": 1,
+                "Safety score": 1,
+                "Operational life": 1,
+                "Cost": 10}  # $/kWh
 
 
 def score(system):
@@ -50,7 +67,9 @@ def score(system):
 
 
 print("Trade-off scores:")
-print("Lithium ion:", score(li_ion))
+print("Lithium-ion:", score(li_ion))
 print("Lithium-sulfur:", score(li_S))
+print("Lithium-metal by SES:", score(li_metal))
 print("Solid state:", score(solid_st))
-print("Hydrogen fuel cell:", score(H2_fuelcell))
+print("High pressure gas hydrogen fuel cell:", score(GH2_fuelcell))
+print("Liquid hydrogen fuel cell:", score(LH2_fuelcell))
