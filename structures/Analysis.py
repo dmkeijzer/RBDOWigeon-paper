5101f1863662cca4fe11fda6cc3bea84558601ff
Kaizad <kaizad@email.com>
2021-06-01 07:58:49 +0000

    Commit to DSE branch main by Kaizad on June  1 at 07:58

--- structures/Analysis.py
+++ structures/Analysis.py
@@ -3,45 +3,52 @@ from MathFunctions.Trigonometry import pi
 from MathFunctions.Mechanics import StepFunction
 import pandas as pd
 
+class Stringer:
+    def __init__(self, area = 0.001, point = (0, 0)):
+        self.a, (self.x, self.y) = area, point
+        
+    __repr__ = __str__ = lambda self: f"Stringer(Area={str(self.a)}, Position={str(self.x), str(self.y)})"
+    
+    Ixx = lambda self: self.a * self.y**2
+    Iyy = lambda self: self.a * self.x**2
+    
 class WingBox:
-    def __init__(self, thickness, base, height, material):
-        self.b, self.h, self.t = base, height, thickness
-        self.mat = material
-        self.beta = {float(d.split('\t')[0]): float(d.split('\t')[1]) for d in """1.0\t0.141
-1.5	0.196
-2.0	0.229
-2.5	0.249
-3.0	0.263
-4.0	0.281
-5.0	0.291
-6.0	0.299
-10.0\t0.312
-10000\t0.333""".split('\n')}
+    def __init__(self, thicknessOfSkin, thicknessOfSpar, base, height, stringers = []):
+        self.b, self.h, self.tsk, self.tsp = base, height, thicknessOfSkin, thicknessOfSpar
+        self.str = stringers
     
-    Area = lambda self: self.b * self.h - (self.b - 2 * self.t) * (self.h - 2 * self.t)
+    __str__ = __repr__ = lambda self: \
+    f"Wingbox(Height={str(self.h)}, Base={str(self.b)}, Tsk = {str(self.tsk)}, Tsp = {str(self.tsp)}, Stringers = {len(self.str)})"
+    
+    Area = lambda self: self.b * self.h - (self.b - 2 * self.tsp) * (self.h - 2 * self.tsk) + sum(s.a for s in self.str)
 
-    Ixx = lambda self: (self.t * self.h ** 3 + self.b * self.t ** 3) / 6 + (self.t * self.b * self.h ** 2) / 2
+    Ixx = lambda self: (self.tsp * self.h ** 3 + self.b * self.tsk ** 3) / 6 + (self.tsk * self.b * self.h ** 2) / 2 + sum(s.Ixx() for s in self.str)
     
-    Iyy = lambda self: (self.t ** 3 * self.h + self.b ** 3 * self.t) / 6 + (self.t * self.h * self.b ** 2) / 2
+    Iyy = lambda self: (self.tsk ** 3 * self.h + self.b ** 3 * self.tsp) / 6 + (self.tsp * self.h * self.b ** 2) / 2 + sum(s.Iyy() for s in self.str)
     
     Vc = Ixy = lambda self: 0
     
+    def StrPlacement(self, nstr_top:int , nstr_bottom:int , area):
+        topstringers = [Stringer(area, point = (i*self.b/(nstr_top + 1) - self.b/2, self.h/2)) for i in range(1, nstr_top+1)]
+        bottomstringers = [Stringer(area, point = (i*self.b/(nstr_bottom + 1) - self.b/2, -self.h/2)) for i in range(1, nstr_bottom+1)]
+        self.str = self.str.copy() + topstringers + bottomstringers
+        
     def Vshear(self, Vy, x, y):
         Ixx = self.Ixx()
         inrge = lambda l1, u1, l2, u2: l1 <= x <= u1 and l2 <= y <= u2
-        vit = - Vy * self.t / Ixx
-        if inrge(0, self.b/2, -self.h/2, -self.h/2 + self.t):
+        vit = - Vy * self.tsp / Ixx if (-self.b/2 <= x <= -self.b/2 + self.tsp) or (self.b/2 - self.tsp <= x <= self.b/2) else - Vy * self.tsk / Ixx
+        if inrge(0, self.b/2, -self.h/2, -self.h/2 + self.tsk):
             return vit * (-self.h * x / 2)
-        elif inrge(self.b/2-self.t, self.b/2, -self.h/2, self.h/2):
+        elif inrge(self.b/2-self.tsp, self.b/2, -self.h/2, self.h/2):
             s = self.h/2 + y
             return vit * (0.5 * s * s - self.h * s / 2) + self.Vshear(Vy, self.b/2, -self.h/2)
-        elif inrge(-self.b/2, self.b/2, self.h/2-self.t, self.h/2):
+        elif inrge(-self.b/2, self.b/2, self.h/2-self.tsk, self.h/2):
             s = self.b/2 - x
             return vit * (self.h*s/2) + self.Vshear(Vy, self.b/2, self.h/2)
-        elif inrge(-self.b/2, -self.b/2+self.t, -self.h/2, self.h/2):
+        elif inrge(-self.b/2, -self.b/2+self.tsp, -self.h/2, self.h/2):
             s = self.h/2 - y
             return vit * (-0.5 * s * s + self.h * s / 2) + self.Vshear(Vy, -self.b/2, self.h/2)
-        elif inrge(-self.b/2, 0, -self.h/2, -self.h/2+self.t):
+        elif inrge(-self.b/2, 0, -self.h/2, -self.h/2+self.tsk):
             return vit * (-self.h * (x + self.b/2) / 2) + self.Vshear(Vy, -self.b/2, -self.h/2)
         else:
             raise ValueError(f"Invalid Coordinates Supplied: {(x, y) = }")
@@ -49,69 +56,33 @@ class WingBox:
     def Hshear(self, Vx, x, y):
         Iyy = self.Iyy()
         inrge = lambda l1, u1, l2, u2: l1 <= x <= u1 and l2 <= y <= u2
-        vit = - Vx * self.t / Iyy
-        if inrge(-self.b/2, -self.b/2+self.t, -self.h/2, 0):
+        vit = - Vx * self.tsp / Iyy if (-self.b/2 <= x <= -self.b/2 + self.tsp) or (self.b/2 - self.tsp <= x <= self.b/2) else - Vx * self.tsk / Iyy
+        if inrge(-self.b/2, -self.b/2+self.tsp, -self.h/2, 0):
             return vit * (self.b * y / 2)
-        elif inrge(-self.b/2, self.b/2, -self.h/2, -self.h/2+self.t):
+        elif inrge(-self.b/2, self.b/2, -self.h/2, -self.h/2+self.tsk):
             s = x + self.b/2
             return vit * (0.5 * s * s - self.b * s / 2) + self.Hshear(Vx, -self.b/2, -self.h/2)
-        elif inrge(self.b/2 -self.t, self.b, -self.h/2, self.h/2):
+        elif inrge(self.b/2 -self.tsp, self.b, -self.h/2, self.h/2):
             return vit * (self.b * (y + self.h/2) / 2) + self.Hshear(Vx, self.b/2, -self.h/2)
-        elif inrge(-self.b/2, self.b/2, self.h/2-self.t, self.h/2):
+        elif inrge(-self.b/2, self.b/2, self.h/2-self.tsk, self.h/2):
             s = -x + self.b/2
             return vit * (-0.5 * s * s + self.b * s / 2) + self.Hshear(Vx, self.b/2, self.h/2)
-        elif inrge(-self.b/2, -self.b/2+self.t, 0, self.h/2):
+        elif inrge(-self.b/2, -self.b/2+self.tsp, 0, self.h/2):
             return vit * (-self.b * (self.h/2-y) / 2) + self.Hshear(Vx, -self.b/2, self.h/2)
         else:
             raise ValueError(f"Invalid Coordinates Supplied: {(x, y) = }")
-
+    
     q = lambda self, x, y, Vx=0, Vy=0, T=0: self.Vshear(Vy, x, y) + self.Hshear(Vx, x, y) + T / (2 * self.Area())
-    tau = lambda self, x, y, Vx=0, Vy=0, T=0: self.q(x, y, Vx, Vy, T) / self.t
+    tau = lambda self, x, y, Vx=0, Vy=0, T=0: self.q(x, y, Vx, Vy, T) / (self.tsp if \
+        (-self.b/2 <= x <= -self.b/2 + self.tsp) or (self.b/2 - self.tsp <= x <= self.b/2) else self.tsk)
+    
     o = lambda self, x, y, Mx=0, My=0: My * x / self.Iyy() + Mx * y / self.Ixx()
-    buckling = lambda self: self.mat.buckling(min(self.b, self.h), self.t)
-    fatigue = lambda self, dS, ai, af: self.mat.ParisFatigueN(dS, self.b, ai, af)
-    def J(self):
-        a, b = max(self.b, self.h), min(self.h, self.b)
-        minerr, closest = float('inf'), -1
-        for val in self.beta:
-            if abs(val - a/b) < minerr:
-                minerr, closest = abs(val - a/b), self.beta[val]
-        return closest * a * b ** 3
 
 class WingStructure:
-    def __init__(self, wingequation):
-        WingLoads = wingequation.SolveEquation()
-        self.RFx, self.RFy, self.RFz = WingLoads[:3]
-        self.RMx, self.RMy, self.RMz = WingLoads[3:] # WingWeight, Lift, Drag, MomentAC, Thrust
-        self.W, self.L, self.D, self.Mac, *self.T = wingequation.k
-
-        self.N = self.Vx = self.Vy = self.Mz = self.Mx = self.My = None
-        self.v = self.w = self.phi = None
-
-    def compute_loading(self):
-        self.N = StepFunction([[self.RFz, 0, 0]])
-        qxzcoef = list(np.polyfit(self.W.p, self.W.v[0] + self.D.v[0] + self.L.v[0], 4))[::-1]
-        qyzcoef = list(np.polyfit(self.W.p, self.W.v[1] + self.D.v[1] + self.L.v[1], 4))[::-1]
-        qxz = StepFunction([[xi, 0, i] for i, xi in enumerate(qxzcoef)])
-        qyz = StepFunction([[yi, 0, i] for i, yi in enumerate(qyzcoef)])
-        self.Vx = StepFunction([[self.RFx, 0, 0]] + [[T.f[0], T.p[2], 0] for T in self.T]) + qxz.integral()
-        self.Vy = StepFunction([[self.RFy, 0, 0]] + [[T.f[1], T.p[2], 0] for T in self.T]) + qyz.integral()
-        self.Mz = self.L.pa[0] * qyz.integral() - self.L.pa[1] * qxz.integral() + self.RMz
-        self.My, self.Mx = self.Vx.integral(-self.RMy), self.Vy.integral(self.RMx)
-        return [self.N, self.Vx, self.Vy, self.Mx, self.My, self.Mz]
-
-    def compute_deflections(self, E, Ixx, Iyy=None):
-        Iyy = Ixx if Iyy is None else Iyy
-        self.v = -self.My.integral().integral() / (E * Ixx)
-        self.w = self.Mx.integral().integral() / (E * Iyy)
-        return self.v, self.w
-
-    def create_nvm(self, b):
-        x = np.linspace(0, b/2, 100)
-        df = pd.DataFrame({'z': x} | {ln: [lv(xi) for xi in x] for ln, lv in \
-                            zip(('v', 'w', 'Mx', 'My', 'Mz', 'N', 'Vx', 'Vy'), 
-                                (self.v, self.w, self.Mx, self.My, self.Mz, self.N, self.Vx, self.Vy))}) \
-                                    .set_index('z')
-        for col in df.columns:
-            df.plot(y=col)
+    def __init__(self, span, taper, rootchord, wingbox):
+        self.span, self.taper, self.rc = span, taper, rootchord
+        self.tc = self.rc * self.taper
+        self.box = wingbox
     
+    chord = lambda self, z: self.rc * (self.taper - 1) * z / (self.span/2) + self.rc if (0 <= z <= self.span/2) else None
+    __call__ = lambda self, z: WingBox(self.box.tsk, self.box.tsp, self.box.b*self.chord(z), self.box.h*self.chord(z), self.box.str)
