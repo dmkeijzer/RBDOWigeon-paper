735db90a1a8b60e1d616828e901c0282db68732b
Kaizad Wadia <58479296+chezzoba@users.noreply.github.com>
2021-06-01 17:53:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from structures.Material import Material
-from structures.SolveLoads import *
+from structures.SolveLoads import SolveACLoads
 
 aluminum = Material.load(file='data/materials.csv', material='Al 6061', Condition='T6')
 
