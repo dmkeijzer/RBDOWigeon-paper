40d2a8ea1a02c1e7150b1cdecbff586b0c04628d
nsalvadorlopez <nsalvadorlopez@student.tudelft.nl>
2021-06-02 17:19:45 +0200

    Weissinger Lifting Line Method - addittion of second wing

--- Preliminary_Lift/Airfoil_analysis.py
+++ Preliminary_Lift/Airfoil_analysis.py
@@ -2,31 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-def airfoil_stats():
-    df1 = pd.read_csv("Airfoil_data/NACA44017_Re2.300.csv")
-    df2 = pd.read_csv("Airfoil_data/NACA44017_Re1.700.csv")
+def airfoil_stats(df1, df2):
+    print(df1)
     df1["cl/cd"] = df1["CL"]/df1["CD"]
 
     Clmax = np.max(df2["CL"])
     Cdmin = np.min(df1["CD"])
     Cl_Cdmin = np.average(df1["CL"][df1["CD"] == Cdmin])
     Cm = np.average(df1["Cm"][df1["CL"] == Cl_Cdmin])
-
-    Clalpha =   (np.average(df1["CL"][df1["alpha"] == 7]) -np.average(df1["CL"][df1["alpha"] == 1]))/6
-    Clalpha1 = (np.average(df1["CL"][df1["alpha"] == 5]) - np.average(df1["CL"][df1["alpha"] == -1])) / 6
-
+    Clalpha =   (np.average(df1["CL"][df1["alpha"] == 3]) +np.average(df1["CL"][df1["alpha"] == -3]))/6
     clcdmax = np.max(df1["cl/cd"])
     Cl_maxld = np.average(df1["CL"][df1["cl/cd"] == clcdmax])
     a_clmax = np.average(df2["alpha"][df2["CL"] == Clmax])
-    a_0L = -np.average(df1["CL"][df1["alpha"] ==0])/Clalpha1
+    a_0L = -np.average(df1["CL"][df1["alpha"] ==0])/Clalpha
 
     return Clmax, Cdmin, Cl_Cdmin, Cm, Clalpha, clcdmax, Cl_maxld, a_clmax, a_0L
-
-def airfoil_datapoint(type, Re, alpha):
-
-    if Re == "Stall":
-        df = pd.read_csv("Airfoil_data/NACA44017_Re1.700.csv")
-    else:
-        df = pd.read_csv("Airfoil_data/NACA44017_Re2.300.csv")
-
-    return np.average(df[type][df["alpha"] == alpha])
+x = airfoil_stats(pd.read_csv("Airfoil_data/LS417_Re2.300.csv"), pd.read_csv("Airfoil_data/LS417_Re1.700.csv"))
+print(
