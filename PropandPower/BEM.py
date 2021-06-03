564f0dee9918efa042f94a021945e4a37c0766b4
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-06-03 13:16:08 +0200

    "Finished" BEM but needs debugging. Also implemented it in the main file

--- PropandPower/BEM.py
+++ PropandPower/BEM.py
@@ -53,12 +53,12 @@ class BEM:
             raise Exception("Please specify either T or P (not both)")
 
     # Prandtl relation for tip loss
-    def F(self, xi, r, zeta):
-        return (2/np.pi) * np.arccos(np.exp(-self.f(xi, r, zeta)))
+    def F(self, r, zeta):
+        return (2/np.pi) * np.arccos(np.exp(-self.f(r, zeta)))
 
     # Exponent used for function above
-    def f(self, xi, r, zeta):
-        return (self.B/2)*(1-xi(r))/(np.sin(self.phi_t(zeta)))
+    def f(self, r, zeta):
+        return (self.B/2)*(1-self.Xi(r))/(np.sin(self.phi_t(zeta)))
 
     # Pitch of blade tip
     def phi_t(self, zeta):
@@ -117,23 +117,23 @@ class BEM:
 
     # Integrals used to calculate internal variables, refer to paper for more explanation if needed
     def I_prim_1(self, xi, zeta, eps):
-        return 4 * xi * (2 / np.pi) * np.arccos(np.exp(-self.B * (1 - zeta) / (2 * np.sin(self.phi_t(zeta))))) * \
+        return 4 * xi * (2 / np.pi) * np.arccos(np.exp(-self.B * (1 - xi) / (2 * np.sin(self.phi_t(zeta))))) * \
                np.cos(np.arctan((1 + zeta / 2) * self.lamb / xi)) * np.sin(np.arctan((1 + zeta/2) * self.lamb/xi)) * \
                (1 - eps(xi) * (1 + zeta / 2) * self.lamb / xi)
 
     def I_prim_2(self, xi, zeta, eps):
-        return self.lamb * (4 / np.pi) * np.arccos(np.exp(-self.B * (1 - zeta) / (2 * np.sin(self.phi_t(zeta))))) * \
+        return self.lamb * (4 / np.pi) * np.arccos(np.exp(-self.B * (1 - xi) / (2 * np.sin(self.phi_t(zeta))))) * \
                np.cos(np.arctan((1 + zeta/2) * self.lamb / xi)) * np.sin(np.arctan((1 + zeta / 2) * self.lamb/xi)) * \
                (1 - eps(xi) * (1 + zeta / 2) * self.lamb / xi) * (1 + xi / ((1 + zeta / 2) * self.lamb / xi)) * \
                np.cos(np.arctan((1 + zeta/2) * self.lamb/xi)) * np.sin(np.arctan((1 + zeta/2) * self.lamb / xi))
 
     def J_prim_1(self, xi, zeta, eps):
-        return 4 * xi * (2 / np.pi) * np.arccos(np.exp(-self.B * (1 - zeta) / (2 * np.sin(self.phi_t(zeta))))) * \
+        return 4 * xi * (2 / np.pi) * np.arccos(np.exp(-self.B * (1 - xi) / (2 * np.sin(self.phi_t(zeta))))) * \
                np.cos(np.arctan((1 + zeta/2) * self.lamb / xi)) * np.sin(np.arctan((1 + zeta/2) * self.lamb / xi)) * \
                (1 + eps(xi) / ((1 + zeta / 2) * (self.lamb / xi)))
 
     def J_prim_2(self, xi, zeta, eps):
-        return 2 * xi * (2 / np.pi) * np.arccos(np.exp(-self.B * (1 - zeta) / (2 * np.sin(self.phi_t(zeta))))) * \
+        return 2 * xi * (2 / np.pi) * np.arccos(np.exp(-self.B * (1 - xi) / (2 * np.sin(self.phi_t(zeta))))) * \
                np.cos(np.arctan((1 + zeta/2) * self.lamb/xi)) * np.sin(np.arctan((1 + zeta / 2) * self.lamb / xi)) * \
                (1 + eps(xi) / ((1 + zeta / 2) * (self.lamb / xi))) * (1 - eps(xi) * (1 + zeta / 2) * self.lamb / xi) * \
                (np.cos(np.arctan((1 + zeta / 2) * self.lamb / xi))) ** 2
@@ -156,17 +156,18 @@ class BEM:
 
     # This function runs the design procedure from an arbitrary start zeta (which can be 0)
     def run_BEM(self, zeta):
-        # Length of each station
-        st_len = (self.R - self.R*self.xi_0)/self.N_s
-
         # Array with station numbers
         stations = np.arange(1, self.N_s + 1)
 
-        # Radius of the middle point of each station
-        stations_r = np.arange(st_len/2, self.R, st_len)
+        # Length of each station
+        st_len = (self.R - self.R*self.xi_0)/len(stations)
+
+        # Radius of the middle point of each station. Station 1 has st length/2, each station has that plus N*st length
+        stations_r = st_len/2 + (stations-1)*st_len
 
         # F and phi for each station
-        F = self.F(self.Xi(stations_r), stations_r, zeta)
+        # self.Xi(stations_r),
+        F = self.F(stations_r, zeta)
         phis = self.phi(stations_r, zeta)
 
         # TODO: Check if Cl range is good
@@ -183,7 +184,7 @@ class BEM:
 
         # Optimise each station for min D/L
         for station in stations:
-
+            station -= 1
             eps_min = 1
             optim_vals = [1, 1, 1, 1]
 
@@ -201,36 +202,90 @@ class BEM:
                 Reyn = self.RN(Wc)
 
                 # Round Reynolds number to 100,000 to retrieve appropriate file from airfoil data folder
-                RN = self.RN_spacing * round(Reyn[station] / self.RN_spacing)
+                RN = self.RN_spacing * round(Reyn / self.RN_spacing)
+
+                # Maximum and minimum RN in database
+                if RN<100000:
+                    RN = 100000
+                if RN>4400000:
+                    RN = 4400000  # TODO update once we have final database
+
                 filename1 = "4412_Re%d_up.txt" % RN
                 filename2 = "4412_Re%d_dwn.txt" % RN
 
                 # TODO: implement code to open and read files
-                file_up = open(filename1, "r")
-                file_down = open(filename2, "r")
+                file_up = open('Airfoil_Data/'+filename1, "r")
+                file_down = open('Airfoil_Data/'+filename2, "r")
 
-                lines = file_up.readlines()
+                # Read lines
+                lines_up = file_up.readlines()
+                lines_down = file_up.readlines()
 
+                # Close files
                 file_up.close()
+                file_down.close()
 
                 # List and Boolean to save relevant lines
                 format_lines = []
                 save_lines = False
 
-                for line in lines:
-
+                for line in lines_up:
+                    # Separate variables inside file
                     a = line.split()
+
+                    # If the save_lines boolean is True (when the code gets to numerical values), save to list
                     if save_lines:
-                        format_lines.append(a)
+                        # Create a line with floats (instead of strings) to append to main list
+                        new_line = []
+                        for value in a:
+                            new_line.append(float(value))
+                        format_lines.append(new_line)
+
+                    # Protect against empty lines
+                    if len(a) > 0:
+                        # There is a line with ---- before the numbers, so once we get to this line, start saving
+                        if a[0].count('-') >= 1:
+                            save_lines = True
+
+                # Restart boolean for down file
+                save_lines = False
+
+                # Do the same process for down file and append to the same array as up
+                for line in lines_down:
+                    a = line.split()
+
+                    if save_lines:
+                        new_line = []
+                        for value in a:
+                            new_line.append(float(value))
+                        format_lines.append(new_line)
+
                     if len(a) > 0:
                         if a[0].count('-') >= 1:
                             save_lines = True
 
-                print(format_lines)
+                # Convert to numpy array with airfoil data
+                airfoil_data = np.array(format_lines)
 
                 # TODO: See format of files and retrieve Cd and AoA from them
-                Cd_ret = 1     # Retrieved Cd
-                alpha_ret = 1  # Retrieved AoA convert from deg to rad
+                # Order airfoil data by angle of attack, this can be eliminated to save time if needed
+                airfoil_data = airfoil_data[airfoil_data[:, 0].argsort()]
+
+                # Get index of line where Cl is the closest to the used value
+
+                # Save airfoil data array to have a copy to modify
+                airfoil_data_check = airfoil_data
+                # Subtract current Cl from list of Cls
+                airfoil_data_check[:, 1] -= lift_coef
+                # Check what line has min Cl difference, and retrieve index of that column
+                index = np.argmin(np.abs(airfoil_data_check[:, 1]))
+
+                # Format of each line:
+                # alpha, CL, CD, Re(CL), CM, S_xtr, P_xtr, CDp
+
+                # Obtain the Cd and AoA from the line where Cl difference is min
+                Cd_ret = airfoil_data[index, 2]                   # Retrieved Cd
+                alpha_ret = airfoil_data[index, 0] * 2*np.pi/180  # Retrieved AoA convert from deg to rad
 
                 # Compute D/L ration
                 eps = Cd_ret / lift_coef
@@ -270,7 +325,7 @@ class BEM:
 
         # TODO: Is this local or general? Check current implementation
         # Possibly implement a function for eps as a function of r/R (xi)
-        eps_fun = spinplt.interp1d(E, stations_r/self.R)
+        eps_fun = spinplt.interp1d(E, stations_r/self.R, fill_value="extrapolate")
 
         # Integrate the derivatives from xi_0 to 1 (from hub to tip of the blade)
         I1 = spint.quad(self.I_prim_1, self.xi_0, 1, args=(zeta, eps_fun))[0]
@@ -283,14 +338,19 @@ class BEM:
             zeta_new = (I1/(2*I2)) - ((I1/(2*I2))**2 - self.Tc/I2)**(1/2)
             Pc = J1*zeta_new + J2*zeta_new**2
 
+            # Propeller efficiency
+            eff = self.efficiency(self.Tc, Pc)
+
+            return zeta_new, [cs, betas, alpha, E, eff, self.Tc, Pc]
+
         elif self.Pc is not None:
             zeta_new = -(J1/(2*J2)) + ((J1/(2*J2))**2 + self.Pc/J2)**(1/2)
             Tc = I1*zeta_new - I2*zeta_new**2
 
-        # Propeller efficiency
-        eff = self.efficiency(Tc, Pc)
+            # Propeller efficiency
+            eff = self.efficiency(Tc, self.Pc)
 
-        return zeta_new, [cs, betas, alpha, E, eff, Tc, Pc]
+            return zeta_new, [cs, betas, alpha, E, eff, Tc, self.Pc]
 
     def optimise_blade(self, zeta_init):
         convergence = 1
@@ -302,12 +362,20 @@ class BEM:
             zeta_new = design[0]
 
             # Check convergence
-            try:
-                convergence = zeta_new / zeta
-            except ZeroDivisionError:
+            if zeta == 0:
                 convergence = zeta_new - zeta
+            else:
+                convergence = zeta_new / zeta
+
+            # This isn't working :(
+            # try:
+            #     convergence = zeta_new / zeta
+            # except ZeroDivisionError:
+            #     convergence = zeta_new - zeta
+
             zeta = zeta_new
 
+        design = self.run_BEM(zeta)
         return design
 
     # TODO
