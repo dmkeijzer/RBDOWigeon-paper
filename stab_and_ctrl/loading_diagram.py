f60a3aa17ce24a3e50ca8b1264c64f3668b7e9b5
jschoser <j.j.schoser@student.tudelft.nl>
2021-06-03 11:16:03 +0100

    Added z-axis to loading diagram

--- stab_and_ctrl/loading_diagram.py
+++ stab_and_ctrl/loading_diagram.py
@@ -2,7 +2,8 @@ class CgCalculator:
     """
     Class to calculate the CG and generate loading diagrams for the aircraft.
     The coordinate system has its origin at the nose, and the x-axis points
-    backwards. The y-axis points towards starboard.
+    backwards. The y-axis points towards starboard and the z-axis points
+    upwards.
 
     @author: Jakob Schoser
     """
@@ -21,11 +22,11 @@ class CgCalculator:
         :param m_cargo: Mass of the cargo
         :param m_pax: Mass of one passenger (incl. personal luggage)
         :param m_pil: Mass of the pilot
-        :param cg_fus: [x, y]-location of the CG of m_fus
-        :param cg_bat: [x, y]-location of the CG of the battery
-        :param cg_cargo: [x, y]-location of the CG of the cargo
-        :param cg_pax: [x, y]-locations of the CGs of all passengers in a list
-        :param cg_pil: [x, y]-location of the CG of the pilot
+        :param cg_fus: [x, y, z]-location of the CG of m_fus
+        :param cg_bat: [x, y, z]-location of the CG of the battery
+        :param cg_cargo: [x, y, z]-location of the CG of the cargo
+        :param cg_pax: [x, y, z]-locations of the CGs of all passengers in a list
+        :param cg_pil: [x, y, z]-location of the CG of the pilot
         """
         self.m_wf = m_wf
         self.m_wr = m_wr
@@ -41,37 +42,42 @@ class CgCalculator:
         self.cg_pax = cg_pax
         self.cg_pil = cg_pil
 
-    def calc_cg(self, x_wf: float, x_wr: float, loaded_cargo: bool,
+    def calc_cg(self, cg_wf: list, cg_wr: list, loaded_cargo: bool,
                 seated_pax: list, seated_pil: bool) -> tuple:
         """
         Calculates the CG of the aircraft for given wing positions and
         occupied seats.
-        :param x_wf: x-location of the CG of the front wing
-        :param x_wr: x-location of the CG of the rear wing
+        :param cg_wf: [x, z]-location of the CG of the front wing
+        :param cg_wr: [x, z]-location of the CG of the rear wing
         :param loaded_cargo: boolean indicating whether cargo has been loaded
-        :param seated_pax: list of booleans indicating passenger presence
+        :param seated_pax: list of passenger indices that are seated
         :param seated_pil: boolean indicating whether pilot is seated
-        :return: [x, y]-location of the CG of the aircraft
+        :return: [x, y, z]-location of the CG of the aircraft
         """
-        x = (self.m_wf * x_wf + self.m_wr * x_wr
+        x = (self.m_wf * cg_wf[0] + self.m_wr * cg_wr[0]
              + self.m_fus * self.cg_fus[0] + self.m_bat * self.cg_bat[0])
-        y = (self.m_fus * self.cg_fus[1] + self.m_bat * self.cg_bat[1])
+        # assume that the CGs of the wings are on the symmetry plane
+        y = self.m_fus * self.cg_fus[1] + self.m_bat * self.cg_bat[1]
+        z = (self.m_wf * cg_wf[1] + self.m_wr * cg_wr[1]
+             + self.m_fus * self.cg_fus[2] + self.m_bat * self.cg_bat[2])
         m = self.m_wf + self.m_wr + self.m_fus + self.m_bat
 
         if loaded_cargo:
             x += self.m_cargo * self.cg_cargo[0]
             y += self.m_cargo * self.cg_cargo[1]
+            z += self.m_cargo * self.cg_cargo[2]
             m += self.m_cargo
 
-        for seated, cg in zip(seated_pax, self.cg_pax):
-            if seated:
-                x += self.m_pax * cg[0]
-                y += self.m_pax * cg[1]
-                m += self.m_pax
+        for seated in seated_pax:
+            x += self.m_pax * self.cg_pax[seated][0]
+            y += self.m_pax * self.cg_pax[seated][1]
+            z += self.m_pax * self.cg_pax[seated][2]
+            m += self.m_pax
 
         if seated_pil:
             x += self.m_pil * self.cg_pil[0]
             y += self.m_pil * self.cg_pil[1]
+            y += self.m_pil * self.cg_pil[2]
             m += self.m_pil
 
         x /= m
@@ -79,14 +85,14 @@ class CgCalculator:
 
         return x, y
 
-    def calc_cg_range(self, x_wf: float, x_wr: float,
-                      order=("cargo", "pil", 1, 2, 3, 4)) -> tuple:
+    def calc_cg_range(self, cg_wf: list, cg_wr: list,
+                      order=("cargo", "pil", 0, 1, 2, 3)) -> tuple:
         """
         Calculate the CG range during loading of the aircraft.
-        :param x_wf: x-location of the CG of the front wing
-        :param x_wr: x-location of the CG of the rear wing
+        :param cg_wf: [x, z]-location of the CG of the front wing
+        :param cg_wr: [x, z]-location of the CG of the rear wing
         :param order: Order of loading different parts. May contain "cargo",
-        "pil", and numbers indicating passenger IDs starting from 1.
+        "pil", and numbers indicating passenger IDs starting from 0.
         :return: [most forward CG, most aft CG],
         [most port CG, most starboard CG]
         """
@@ -94,7 +100,7 @@ class CgCalculator:
         x_front, x_aft, y_port, y_star = None, None, None, None
 
         loaded_cargo = False
-        seated_pax = [False for _ in order]
+        seated_pax = []
         seated_pil = False
 
         for item in order:
@@ -103,9 +109,9 @@ class CgCalculator:
             elif item == "pil":
                 seated_pil = True
             else:
-                seated_pax[item - 1] = True
+                seated_pax.append(item)
 
-            x, y = self.calc_cg(x_wf, x_wr, loaded_cargo,
+            x, y = self.calc_cg(cg_wf, cg_wr, loaded_cargo,
                                 seated_pax, seated_pil)
 
             if x_front is None:
