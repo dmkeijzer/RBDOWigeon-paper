cbe12226bceec47116342834ba34311951fefd67
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-06-04 12:50:02 +0200

    Cruise power

--- PropandPower/main_PnP_final.py
+++ PropandPower/main_PnP_final.py
@@ -8,17 +8,43 @@ a = ISA.soundspeed()
 rho = ISA.density()
 dyn_visc = ISA.viscosity_dyn()
 
+
+# Cessna 172
+# # B, R, rpm, xi_0, rho, dyn_vis, V_fr, N_stations, a, RN_spacing, T=None, P=None
+# B = 4
+# xi_0 = 0.05
+# # A_prop = 0.47*2
+# # R = np.sqrt(A_prop / (np.pi * (1 - xi_0**2)))
+# R = 0.95
+# A_prop = np.pi*R**2
+#
+# # M_t_max = 0.6
+# # rpm = M_t_max*a*60 / (np.pi * 2*R)
+# rpm = 2700
+#
+# V_cruise = 62
+# N_stations = 20
+# RN_spacing = 100000
+#
+# # P_cr = 110024
+# # T = (P_cr*2*np.sqrt(rho*8*A_prop) * 0.65)**(2/3)
+#
+# # Cessna 172, CdS = 5.93 sq ft
+# D = 0.551*rho*V_cruise**2/2
+# T = D
+
+
 # B, R, rpm, xi_0, rho, dyn_vis, V_fr, N_stations, a, RN_spacing, T=None, P=None
-B = 4
+B = 5
 xi_0 = 0.05
 # A_prop = 0.47*2
 # R = np.sqrt(A_prop / (np.pi * (1 - xi_0**2)))
-R = 0.95
+R = 0.6
 A_prop = np.pi*R**2
 
 # M_t_max = 0.6
 # rpm = M_t_max*a*60 / (np.pi * 2*R)
-rpm = 2700
+rpm = 3500
 
 V_cruise = 62
 N_stations = 20
@@ -28,14 +54,17 @@ RN_spacing = 100000
 # T = (P_cr*2*np.sqrt(rho*8*A_prop) * 0.65)**(2/3)
 
 # Cessna 172, CdS = 5.93 sq ft
-D = 0.551*rho*V_cruise**2/2
+D = 0.351*rho*V_cruise**2/2
 T = D
 
 propeller = BEM.BEM(B, R, rpm, xi_0, rho, dyn_visc, V_cruise, N_stations, a, RN_spacing, T=T)
 
 # Zeta init
 zeta_init = 0
-design = propeller.optimise_blade(zeta_init)[1]
+zeta, design = propeller.optimise_blade(zeta_init)
+
+print("Displacement velocity (zeta):", zeta)
+print("")
 # [cs, betas, alpha, E, eff, Tc, self.Pc]
 print("Chord per station:", design[0])
 print("")
