66754847f294f5df313f6158572077e809582d79
mcuadratgrzybo <mcuadratgrzybo@student.tudelft.nl>
2021-06-04 11:12:43 +0200

    In Scissor_Plots.py vertical drag position changed

--- stab_and_ctrl/main.py
+++ stab_and_ctrl/main.py
@@ -9,10 +9,10 @@ from matplotlib import pyplot as plt
 # example values based on inputs_config_1.json
 W = 18315.27
 h = 0
-lfus = 4
-hfus = 1.6
-wfus = 1.3
-xcg = 1.680
+lfus = 7.2
+hfus = 1.705
+wfus = 1.38
+xcg = 3.0
 V0 = 52
 Vstall = 40
 M0 = V0 / np.sqrt(const.gamma * const.R * 288.15)
@@ -31,13 +31,13 @@ Cmacrear = -0.0645
 S = 10.5
 Sfwd = 1/2*S
 Srear = S-Sfwd
-Afwd = 7
-Arear = 7
+Afwd = 7*2
+Arear = 7*2
 Gamma = 0
 Lambda_c2_fwd = 0
 Lambda_c2_rear = 0
-cfwd = 0.8748
-crear = 0.8748
+cfwd = 0.65
+crear = 0.65
 bfwd = np.sqrt(Sfwd * Afwd)
 brear = np.sqrt(Srear * Arear)
 efwd = 1.1302
@@ -51,12 +51,12 @@ K = 4959.86
 ku = 0.1
 Zcg = 0.7
 
-
-wps = Wing_placement_sizing(W, h, lfus, hfus, wfus, V0, M0, CD0, theta0, CLfwd,
+d = 1.5
+wps = Wing_placement_sizing(W,  lfus, hfus, wfus, V0, M0, CD0, CLfwd,
                  CLrear, CLafwd, CLarear, Cmacfwd, Cmacrear, Sfwd, Srear,
                  Afwd, Arear, Gamma, Lambda_c2_fwd, Lambda_c2_rear, cfwd,
                  crear, bfwd, brear, efwd, erear, taper, n_rot_f, n_rot_r,
-                 rot_y_range_f, rot_y_range_r, K, ku,Zcg)
+                 rot_y_range_f, rot_y_range_r, K, ku,Zcg,d)
 
 vt_sizing = VT_sizing(W,h,xcg,lfus,hfus,wfus,V0,Vstall,M0,CD0,theta0,
                       CLfwd,CLrear,CLafwd,CLarear,
@@ -65,12 +65,11 @@ vt_sizing = VT_sizing(W,h,xcg,lfus,hfus,wfus,V0,Vstall,M0,CD0,theta0,
 elevon = Control_surface(V0,Vstall,CLfwd,CLrear,CLafwd,CLarear,Clafwd,Clarear,Cd0fwd,Cd0rear,
                          Sfwd,Srear,Afwd,Arear,cfwd,crear,bfwd,brear,taper)
 elevator_effect = 1.4
-d = 0
 dx = 0.1
 
 #### Plotting Vertical Tail ####
-nE = 8
-Tt0 = 3500
+nE = 16
+Tt0 = 7000
 yE = bfwd/2
 lv = lfus-xcg
 brbv = np.linspace(0.75,1,150)
