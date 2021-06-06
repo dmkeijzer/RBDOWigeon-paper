20c5e59a993d45f36c944e3ac06a0ee8e7283f67
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-06-06 19:22:02 +0200

    Fixed bug in engine sizing and created file to redo midterm

---
+++ Midterm2.0_for_convergence/main_remidterm.py
@@ -0,0 +1,5 @@
+"""
+Here we will run the midterm code (and part of the new code when available) to have converged values
+Create copies of the midterm tools in this folder if necessary and make sure they comply with the new coding rules
+Among other things, the idea is to start with a higher weight estimate, closer to the current estimated weight
+"""
