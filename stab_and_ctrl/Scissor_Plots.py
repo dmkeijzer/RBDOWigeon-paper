00143e9fa574ae431fc5d007ddbf08f6c232a57a
mcuadratgrzybo <mcuadratgrzybo@student.tudelft.nl>
2021-06-08 16:01:04 +0200

    main.py merged, Stability_derivatives.py

--- stab_and_ctrl/Scissor_Plots.py
+++ stab_and_ctrl/Scissor_Plots.py
@@ -12,7 +12,7 @@ class Wing_placement_sizing:
                  CLrear,CLdesfwd,CLdesrear,Clafwd,Clarear, Cmacfwd, Cmacrear, Sfwd, Srear,
                  Afwd, Arear, Gamma, Lambda_c4_fwd, Lambda_c4_rear, cfwd,
                  crear, bfwd, brear, efwd, erear, taper, n_rot_f, n_rot_r,
-                 rot_y_range_f, rot_y_range_r, K, ku,Zcg,d,dy,Pbr):
+                 rot_y_range_f, rot_y_range_r, K, ku,Zcg,d,dy,Pbr,Vr_V2):
 
         self.W = W         # Weight [N]
         self.h = h       # Height [m]
@@ -62,6 +62,7 @@ class Wing_placement_sizing:
         self.CLdesrear = CLdesrear
         self.de_da = self.deps_da(self.Sweepc4fwd,self.bfwd,self.lh(),self.hfus-self.dy,self.Afwd,self.CLafwd)
         # print("de/da = %.3f"%(self.de_da))
+        self.Vr_V2 = Vr_V2
         self.Zcg = Zcg
         self.hover_calc = HoverControlCalcTandem(W / consts.g, n_rot_f,
                                                  n_rot_r, self.xacfwd,
@@ -91,10 +92,14 @@ class Wing_placement_sizing:
         phi = np.arcsin(mtv/r)
         # print("r, mtv = ",r,mtv)
         # print("phi = %.3f"%(phi*180/np.pi))
-        if 180/np.pi*phi<30 and 180/np.pi*phi>0: #To account for propeller downwash
-            dsde_da = 6.5*(self.rho*self.Pbr**2*self.Sfwd**3*self.CLdesfwd**3/(self.lh()**4*self.W**3))**(1/4)*(np.sin(phi*6))**2.5
-        else:
-            dsde_da = 0
+        # if 180/np.pi*phi<30 and 180/np.pi*phi>0: #To account for propeller downwash
+        #     dsde_da = 6.5*(self.rho*self.Pbr**2*self.Sfwd**3*self.CLdesfwd**3/(self.lh()**4*self.W**3))**(1/4)*(np.sin(phi*6))**2.5
+        # else:
+        #     dsde_da = 0
+        Sf = b**2/A
+        dsde_da = np.where(np.logical_and(np.rad2deg(phi) < 30, np.rad2deg(phi) > 0),
+            6.5 * (self.rho * self.Pbr ** 2 * Sf ** 3 * self.CLdesfwd ** 3 / (lh ** 4 * self.W ** 3)) ** (1 / 4) * (np.sin(phi * 6)) ** 2.5,
+            0)
         # print("Configuration %.0f de/da = %.4f "%(conf,de_da))
         return de_da+dsde_da
 
@@ -114,7 +119,6 @@ class Wing_placement_sizing:
     def C_L_a(self,A, Lambda_half, eta=0.95):
         """
         Inputs:
-        :param M: Mach number
         :param b: wing span
         :param S: wing area
         :param Lambda_half: Sweep angle at 0.5*chord
@@ -150,19 +154,45 @@ class Wing_placement_sizing:
         return SrSfwd_stab ** (-1), SrSfwd_control ** (-1)
 
     def plotting(self, x_min, x_max, dx, elevator, d, n_failures=2, y_cg=0):
+        Sr_Sfwd = np.linspace(0, 2.5, 100)
+        Sforward = self.S*(1/(1+Sr_Sfwd))
+        Sr = self.S- Sforward
+        cforward = self.cfwd*np.sqrt(Sforward/self.Afwd)/np.sqrt(self.Sfwd/self.Afwd)
+        cr = self.crear*np.sqrt(Sr/self.Arear)/np.sqrt(self.Srear/self.Arear)
+        xacfwd_1 = 0.25*cforward + self.d
+        xacrear_1 = self.lfus - (1 - 0.25) * cr
+        # xacfwd_1 = self.xacfwd
+        # xacrear_1 = self.xacrear
+        # CLfwd = self.CLfwd * elevator
+        CDfwd = self.CD0 + self.CLfwd ** 2 / (np.pi * self.Afwd * self.efwd)
+        CDrear = self.CD0 + self.CLrear ** 2 / (np.pi * self.Arear * self.erear)
+        # c = self.Sfwd / (self.Sfwd + self.Srear) * self.cfwd + self.Srear / (self.Srear + self.Sfwd) * self.crear
+        CDafwd = 2 * self.CLafwd * self.CLdesfwd / (np.pi * self.Afwd * self.efwd)
+        CDarear = 2 * self.CLarear * self.CLdesrear / (np.pi * self.Arear * self.erear)
+        # print("CD_alpha = ",CDafwd)
+        deda = self.deps_da(self.Sweepc4fwd,np.sqrt(Sforward*self.Afwd),xacrear_1-xacfwd_1,self.hfus-self.dy,self.Afwd,self.CLafwd)
+        o = -self.Cmacfwd*cforward+ CDfwd*(self.Zcg-self.dy)+self.CLfwd*elevator*xacfwd_1-\
+            self.Vr_V2*(CDrear*(self.hfus-self.Zcg)*Sr_Sfwd-self.CLrear*Sr_Sfwd*xacrear_1+self.Cmacrear*Sr_Sfwd*cr)
+        p = self.CLfwd*elevator+self.Vr_V2*self.CLrear*Sr_Sfwd
+        xcg_control = o/p
+
+        oo = CDafwd*(self.Zcg-self.dy)+self.CLafwd*xacfwd_1-\
+            self.Vr_V2*(CDarear*(self.hfus-self.Zcg)*(1-deda)*Sr_Sfwd-self.CLarear*Sr_Sfwd*xacrear_1*(1-deda))
+        pp = self.CLafwd + self.Vr_V2 * self.CLarear * Sr_Sfwd*(1-deda)
+        xcg_stability = oo/pp
         Xcg = np.arange(x_min, x_max, dx)
         SrSfwd_stability = self.Sr_Sfwd(Xcg,elevator,d)[0]
         SrSfwd_control = self.Sr_Sfwd(Xcg, elevator, d)[1]
-        plt.plot(Xcg,SrSfwd_stability,label="Neutral Stability Line")
-        plt.plot(Xcg,SrSfwd_control,label="Controllability Line")
+        plt.plot(Sr_Sfwd,xcg_stability,label="Neutral Stability Line")
+        plt.plot(Sr_Sfwd,xcg_control,label="Controllability Line")
 
         # x_hover_min, x_hover_max, hover_fail_limit_front, hover_fail_limit_aft = self.hover_calc.calc_crit_x_cg_range(x_min, x_max, dx, y_cg, [(self.xacfwd + self.xacrear) / 2, 0], [n_failures])[0]
         # plt.axvline(x_hover_min, label="Hover Controllability Limits for " + str(n_failures) + " Engine Failures", color="tab:red")
         # plt.axvline(x_hover_max, color="tab:red")
 
-        plt.xlabel(r"$x_{cg}$ [m]",fontsize=14)
-        plt.ylabel(r"$\frac{S_{fwd}}{S_{rear}}$ [-]",fontsize=14)
-        plt.ylim(0.5, 1.5)
+        plt.ylabel(r"$x_{cg}$ [m]",fontsize=14)
+        plt.xlabel(r"$\frac{S_{rear}}{S_{fwd}}$ [-]",fontsize=14)
+        # plt.ylim(0.5, 1.5)
         plt.legend()
 
         plt.show()
