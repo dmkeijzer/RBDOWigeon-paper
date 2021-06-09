40e603a3d6d966271c1d2c6893e3b4cbdd64b1b9
Javier <57629389+JavierAlbaM@users.noreply.github.com>
2021-06-10 01:25:56 +0200

    Final midterm code for CAD

--- PropandPower/power_budget.py
+++ PropandPower/power_budget.py
@@ -3,11 +3,11 @@ File created for incorporating other systems that require power
 """
 
 class Power_Budget:
-    def __init__(self):
+    def __init__(self, m):
         # Constant required power
         self.avionics = 252  # [W]
         self.airco = 3000  # [W]
-        self.battery_cooling = 30 * 6  # [W]
+        self.battery_cooling = 30 * m/72  # [W]
         self.autopilot = 151  # [W]
         self.trim = 54  # [W]
         self.passenger_power = 400  # [W]
@@ -16,7 +16,7 @@ class Power_Budget:
 
         # Non continuous power
         self.landing_gear = 300  # [W]
-        self.wing_rot_mech = 3000 # [W]
+        self.wing_rot_mech = 3000  # [W]
 
     def P_continuous(self):
         return self.avionics + self.airco + self.battery_cooling + self.autopilot + self.trim + self.passenger_power \
