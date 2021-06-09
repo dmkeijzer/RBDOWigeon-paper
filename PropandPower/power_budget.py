250d578505328e4d4723e7b65ac652b06698e53a
KoenPrudhommevanReine <koen.prudhomme@ziggo.nl>
2021-06-09 13:05:12 +0200

    Start of power budget

--- PropandPower/power_budget.py
+++ PropandPower/power_budget.py
@@ -2,13 +2,22 @@
 File created for incorporating other systems that require power
 """
 
+
 class power_budget:
     def __init__(self):
-        self.avionics = 252
-        self.airco = 3000
-        self.battery_cooling = 30*6
-        self.autopilot = 151
-        self.trim = 54
-        self.landing_gear = 300
-        self.passenger_power = 400
-        self.external_lights = 117
+        # Constant required power
+        self.avionics = 252  # [W]
+        self.airco = 3000  # [W]
+        self.battery_cooling = 30 * 6  # [W]
+        self.autopilot = 151  # [W]
+        self.trim = 54  # [W]
+        self.passenger_power = 400  # [W]
+        self.external_lights = 117  # [W]
+        self.deice = 3000  # [W]
+
+        # Non continuous power
+        self.landing_gear = 300  # [W]
+        self.wing_rot_mech = 3000 # [W]
+
+    def P_continuous(self):
+        return
