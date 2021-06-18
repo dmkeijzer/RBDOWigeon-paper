6dd19b5b2a4486a73cf14cc388bf2e42f990cf52
alexm <A.MontoyaSantamaria@student.tudelft.nl>
2021-06-18 14:02:02 +0200

    main aero w new data

--- Preliminary_Lift/main_aero.py
+++ Preliminary_Lift/main_aero.py
@@ -23,11 +23,11 @@ datafile.close()
 FP = data["Flight performance"]
 STR = data["Structures"]
 #AR = 3.75
-AR1 = 7.25
-AR2=  7.85
+AR1 = 6.8
+AR2=  6.8
 
-W = 3144 #STR["MTOW"] #[N]
-Vcruise = 70#FP["V_cruise"] #[m/s]
+W = 3024.8 #STR["MTOW"] #[N]
+Vcruise = 72.18#FP["V_cruise"] #[m/s]
 Wing_loading = FP["WS"]
 
 #Cruise conditions
@@ -39,11 +39,11 @@ a = atm_flight.soundspeed()
 M = Mach(Vcruise,a)
 print("Mach",M)
 #Wing planform
-S_ref = 26.9148 #W/Wing_loading #[m**2] PLACEHOLDER
+S_ref = 19.8213 #W/Wing_loading #[m**2] PLACEHOLDER
 
 
 # For double wing
-s1=0.454545454545
+s1=0.5
 
 s2=1-s1
 b = np.sqrt(0.5*(AR1*s1+s2*AR2)*S_ref) # Due to reqs
@@ -54,12 +54,12 @@ sweepc42=0
 #Other paramters
 b_d = b  # fixed due to span limitations
 h_d = 1.4  #  Vertical gap between wings. Based on fuselage size
-l_h = 7 # Horizontal gap between wings. Based on fuselgae size
+l_h = 5.6 # Horizontal gap between wings. Based on fuselgae size
 i1 = -0.0
 
 #Fuselage dimensions
 l1 = 2.5
-l2 = 2.5
+l2 = 2.14888
 l3 = 2.7
 w_max = 1.38
 h_max = 1.70
@@ -77,7 +77,7 @@ C_t = Wing_params.wing_planform_double()[0][2]
 MAC = Wing_params.wing_planform_double()[1][3]
 print("YE",b, C_r, C_t, MAC)
 SweepLE = Wing_params.sweep_atx(0)[0]
-deda = 0.25 #downwash(Wing_params.wing_planform_double()[0][0] , Wing_params.AR1,
+deda = 0.352 #downwash(Wing_params.wing_planform_double()[0][0] , Wing_params.AR1,
               #  Wing_params.wing_planform_double()[0][1], Wing_params.wing_planform_double()[0][2], Wing_params.sweepc41, 5, Wing_params.h_ht,
               #  Wing_params.lh, Wing_params.wing_planform_double()[1][0] ,
               #  Wing_params.wing_planform_double()[0][1], Wing_params.wing_planform_double()[0][2], Wing_params.sweepc41,
@@ -103,9 +103,9 @@ tc = 0.12 #NACA0012 for winglets and Vtail
 tca = 0.17
 xcm = 0.3 #NACA0012 for winglets and Vtail
 CL_CDmin = airfoil[2]
-CL_lst = np.arange(-0.5,1.7,0.100)
+CL_lst = np.arange(-0.5,1.8,0.025)
 #Other parameters
-S_v = 0.6
+S_v = 0.614
 S_t = 0
 
 
@@ -113,11 +113,28 @@ Drag = componentdrag('tandem',S_ref,l1,l2,l3,d_eq,Vcruise,rho,MAC,AR1,AR2,Mach(V
 
 
 CL_design = Drag.CL_des()
-Cd_des= Drag.Cd_w(CL_design[0])
+print("Cl_des", CL_design/(np.cos(SweepLE)**2))
+fig2 , ax = plt.subplots(1,2)
+Cd= Drag.Cd_w(CL_lst*np.cos(SweepLE)**2)/1.1
+print(Cd)
+CD = Drag.CD(CL_lst)
+print(CD)
+ax[0].plot(CL_lst, Cd)
+ax[0].set_xlabel('$C_l [-]$', fontsize = 16)
+ax[0].set_ylabel("$C_d [-]$", fontsize = 16)
+ax[1].plot(CL_lst, CD)
+ax[1].set_xlabel('$C_L [-]$', fontsize = 16)
+ax[1].set_ylabel("$C_D [-]$", fontsize = 16)
+plt.show()
+
+fig1, ax1 = plt.subplots()
+
+drag_comp = [Drag.CD0_f*1.05, Drag.CD0_v*1.05, Drag.CD_upsweep(), Drag.Cd_w(0.492),Drag.CDi(0.492) ]
+
 Swet_f = Drag.Swet_f()
 
 Rey = Re(rho, Vcruise,MAC, mu)
-print("Re", Rey)
+#print("Re", Rey)
 #Stall
 stall = Wing_params.CLmax_s(deda)
 CLmax = stall[0]
@@ -135,23 +152,28 @@ Cl_alpha_curve = Wing_params.CLa(tca, CDs, CDs_f, Afus, alpha_lst, deda)[0]
 CD_a_w = Wing_params.CDa_poststall(tca, CDs_w, CDs_f, Afus, alpha_lst, "wing", Drag.CD, deda)
 CD_a_f = Wing_params.CDa_poststall(tca, CDs_w, CDs_f, Afus, alpha_lst, "fus", Drag.CD, deda)
 
-plt.plot(alpha_lst, Cl_alpha_curve)
+fig , ax = plt.subplots(1,3)
+ax[0].plot(alpha_lst, Cl_alpha_curve)
+ax[0].set_xlabel(r'$\alpha [^o]$', fontsize = 16)
+ax[0].set_ylabel("$C_L [-]$", fontsize = 16)
+ax[1].plot(alpha_lst, CD_a_w)
+ax[1].set_xlabel(r'$\alpha [^o]$', fontsize = 16)
+ax[1].set_ylabel("$C_{D_w} [-]$", fontsize = 16)
+ax[2].plot(alpha_lst, CD_a_f)
+ax[2].set_xlabel(r'$\alpha_f [^o]$', fontsize = 16)
+ax[2].set_ylabel("$C_{D_f} [-]$", fontsize = 16)
 plt.show()
 
 #x = optimize_wingtips(0,0.2,0.005, 1.5, 'tandem',S_ref,l1,l2,l3,d_eq,Vcruise,rho,MAC,AR,Mach(Vcruise,a),k,flamf,flamw,mu,tc,xcm,0,SweepLE,u,C_t,h_d,IF_f,IF_w, IF_v, CL_CDmin,Abase, S_v, s1, s2)
 #                  Drag = componentdrag('tandem',S_ref,l1,l2,l3,d_eq,Vcruise,rho,MAC,AR,Mach(Vcruise,a),k,flamf,flamw,mu,tc,xcm,0,SweepLE,u,C_t,h_d,IF_f,IF_w, IF_v, CL_CDmin,Abase, S_v, s1, s2, h_wl1, h_wl1)
-#
-#print(x)
 
-#x = LLT2wings(b,2*AR,C_r,C_t,0,5, h_d, 6,b,2*AR,C_r,C_t,0,5, 70, 0, 1.5)
-#print(b,C_t, C_r)
-#x = LLT1wing
+
 #print(x)
 D = 0.965 #m
-T = 240.5 #N
+T = 153.5 #N
 ne1 = 6
 ne2 = 6
-
+"""
 #152.34774175721694 46.21428413797744 1.111617926993772 0.8934300657546558 6 6 0.17 0.17230950765784175 0.004628973850025598 1.8425440913304136 1 0.2723339479180486
 alpha_wp = np.arange(-5,18,0.25)
 Cl_alpha_curve2 = Wing_params.CLa(tc, CDs, CDs_f, Afus, alpha_wp, deda)
@@ -170,10 +192,10 @@ plt.show()
 plt.plot(alpha_wp, CLwp[7])
 plt.plot(alpha_wp, Cl_alpha_curve2[2])
 plt.show()
+"""
 
 
-
-
+"""
 def plot_stagger_gap(b, CL, S):
     gap = np.linspace(1,1.5,50)
     stagger = np.linspace(1,7,50)
@@ -201,5 +223,5 @@ def plot_stagger_gap(b, CL, S):
 
 plot_stagger_gap(7, 0.4, 16) # plot_stagger_gap(b, 0.4, S_ref)
 
-
+"""
 
