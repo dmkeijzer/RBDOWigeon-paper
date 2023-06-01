86f590b832dedbc06653834c4d13268d72fc0d80
Damien Keijzer <damien.keijzer@gmail.com>
2023-06-01 21:45:55 +0200

    Added scv_baseline reader to compare with the result of wigeon

---
+++ Validation_analysis/csv_baseline_reader.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+file_path = r"C:\Users\damie\OneDrive\Desktop\Damien\Wigeon_proj\logs\valid_data\Baseline\Deterministic_Jun__1_00.15_hist.csv"
+
+
+data_csv = pd.read_csv(file_path)
+conv_lst = data_csv["converged"]
+
+fig, axs = plt.subplots(3,3)
+
+#MTOM
+y1 = data_csv["MTOM"][conv_lst].to_numpy()
+axs[0, 0].plot(y1, label= "MTOM")
+axs[0, 0].legend()
+axs[0, 0].set_xlabel(r"$n_{th}$ Converged design")
+axs[0, 0].set_ylabel('MTOM [kg]')
+
+# Aspect ratio's
+y2 = data_csv["AR1"][conv_lst].to_numpy()
+y3 = data_csv["AR2"][conv_lst].to_numpy()
+axs[0, 1].plot(y2, label= "AR front")
+axs[0, 1].plot(y3, label= "AR rear")
+axs[0, 1].set_xlabel(r"$n_{th}$ Converged design")
+axs[0, 1].set_ylabel("AR [-]")
+axs[0, 1].legend()
+
+# Cm alpha
+y4 = data_csv["Cm_alpha"][conv_lst].to_numpy()
+axs[0, 2].plot(y4, label=r'$C_{m_{\alpha}}$' )
+axs[0, 2].set_xlabel(r"$n_{th}$ Converged design")
+axs[0, 2].set_ylabel(r'$C_{m_{\alpha}}$')
+axs[0, 2].legend()
+
+# CLmax
+y5 = data_csv["CLmax"][conv_lst].to_numpy()
+axs[1, 0].plot(y5, label=r'$C_{Lmax}$ [-]')
+axs[1, 0].set_xlabel(r"$n_{th}$ Converged design")
+axs[1, 0].set_ylabel(r'$C_{Lmax}$ [-]')
+axs[1, 0].legend()
+
+# Control margin
+y6 = data_csv["ctrl_margin"][conv_lst].to_numpy()
+axs[1, 1].plot(y6,label=r'$x_{marg}$')
+axs[1, 1].set_xlabel(r"$n_{th}$ Converged design")
+axs[1, 1].set_ylabel(r'$x_{marg}$ [m]')
+axs[1, 1].legend()
+
+# S total
+y7 = data_csv["S1"][conv_lst].to_numpy()  
+y8 = data_csv["S2"][conv_lst].to_numpy()
+axs[1, 2].plot(y7, label= r"$S_{front}$")
+axs[1, 2].plot(y8, label= r"$S_{rear}$")
+axs[1, 2].set_xlabel(r"$n_{th}$ Converged design")
+axs[1, 2].set_ylabel(r'S [$m^2$]')
+axs[1, 2].legend()
+
+
+plt.show()
